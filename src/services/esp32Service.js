d96cf5b405b2c327088b4fbe69389b124e86e328
Rin <rin31426@gmail.com>
2026-04-19 18:19:39 +0300

    reports

--- src/services/esp32Service.js
+++ src/services/esp32Service.js
@@ -18,7 +18,7 @@ class ESP32Service {
         this.isPolling = true;
         this.pollingInterval = setInterval(async () => {
             await this.fetchLatestReading();
-        }, 5000);
+        }, 500000);
         console.log('📡 ESP32 Service: Polling started');
     }
 
